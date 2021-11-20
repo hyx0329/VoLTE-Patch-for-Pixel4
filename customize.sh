af8f3df28a75469d6b36669ea515a4c82dae4a64
hyx0329 <hyx0329@163.com>
2021-11-20 21:02:55 +0800

    fix file detection bug

--- customize.sh
+++ customize.sh
@@ -13,6 +13,6 @@ FILES_TO_REMOVE="
 /data/vendor/modem_fdr/fdr_check
 "
 for i in $FILES_TO_REMOVE; do
-    ui_print "- removing file: $i"
-    [ -f $i ] && rm -rf $i
+    ui_print "- Try to remove file: $i"
+    [ -e $i ] && rm -rf $i && ui_print "- Remove $i success" || ui_print "- Remove $i failed"
 done
